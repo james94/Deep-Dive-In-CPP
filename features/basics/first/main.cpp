4acf2d265fed9a2a2a681d25a0ad5e3fca392baf
james94 <jamesmedel94@gmail.com>
2020-07-18 15:47:31 -0700

    Created First C++ Program
    Programs requests 2 numbers from keyboard, user enters 2 numbers,
    then program adds them together and prints the result to the screen

---
+++ features/basics/first/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Monolithic Program
+
+    // Ex 1: Take 2 nums from keyboard and add them
+    int a, b, c;
+    cout << "Enter 2 nums";
+    cin >> a >> b;
+    c = a + b;
+    // Then print them to the screen
+    cout << "Sum is " << c << endl;
+    return 0;
+}
