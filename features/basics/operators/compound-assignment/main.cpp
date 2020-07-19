b374948be350b0859897ffa5b5e75debbc077e22
james94 <jamesmedel94@gmail.com>
2020-07-19 14:50:05 -0700

    Created Compound Assignment Program

---
+++ features/basics/operators/compound-assignment/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// cplusplus ref: Operators
+// url: http://www.cplusplus.com/doc/tutorial/operators/
+
+// Compound Assignment
+// +=
+// -=
+// *=
+// /=
+// %=
+// &=
+// |=
+// <<=
+// >>=
+// ^=
+
+int main() {
+    int sum = 10;
+    int x = 5;
+    int y = 1;
+    sum += x;
+    cout << sum << endl;
+
+    y += 1;
+    cout << y << endl;
+    return 0;
+}
