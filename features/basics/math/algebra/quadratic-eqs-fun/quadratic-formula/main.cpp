755b0eabbd0117e7220694c57eaa77073e6d357f
james94 <jamesmedel94@gmail.com>
2020-07-19 08:05:28 -0700

    Updated Quadratic Formula Program with two root results

--- features/basics/math/algebra/quadratic-eqs-fun/quadratic-formula/main.cpp
+++ features/basics/math/algebra/quadratic-eqs-fun/quadratic-formula/main.cpp
@@ -6,20 +6,23 @@ using namespace std;
 // url: https://www.khanacademy.org/math/algebra-home/alg-quadratics#alg-solving-quadratics-using-the-quadratic-formula
 
 // Finding roots of a quadratic equation
-float root(int a, int b, int c);
+void root(int a, int b, int c);
 
 int main() {
     int a = 3, b = 12, c = 8;
-    cout << root(a, b, c) << endl;
+    cout << "Enter 3 values";
+    cin >> a >> b >> c;
+    root(a, b, c);
     return 0;
 }
 
-float root(int a, int b, int c)
+void root(int a, int b, int c)
 {
-    float root1;
+    float root1, root2;
 
     // root1 of the quadratic formula
     root1 = (-b + sqrt(b*b - 4*a*c))/(2*a);
+    root2 = (-b - sqrt(b*b - 4*a*c))/(2*a);
 
-    return root1;
+    cout << root1 << " " << root2 << endl;
 }
