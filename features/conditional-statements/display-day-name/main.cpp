c607162bb6d63953750fb871943c92d671a1beb7
james94 <jamesmedel94@gmail.com>
2020-07-26 18:46:23 -0700

    Added switch case approach to DayName Program

--- features/conditional-statements/display-day-name/main.cpp
+++ features/conditional-statements/display-day-name/main.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 // Display day name for given day number
-void DayName(int day);
+void DayNameElseIfLadder(int day);
+
+void DayNameSwitchCase(int day);
 
 int main() {
     int day;
     cout << "Enter day number";
     cin >> day;
-    DayName(day);
+//    DayNameElseIfLadder(day);
+    DayNameSwitchCase(day);
     return 0;
 }
 
-void DayName(int day)
+void DayNameElseIfLadder(int day)
 {
 
     //Display day name in 3 letters using else if ladder
@@ -47,5 +50,40 @@ void DayName(int day)
     else
     {
         cout << "invalid day";
+    }
+}
+
+// Faster than else if ladder because switch case will look at the number
+// passed into switch(num) and then jump directly to that case instead of
+// checking each condition like else if ladder
+void DayNameSwitchCase(int day)
+{
+    //Display day anem using switch
+    switch(day)
+    {
+        case 1:
+            cout << "sun";
+            break;
+        case 2:
+            cout << "mon";
+            break;
+        case 3:
+            cout << "tue";
+            break;
+        case 4:
+            cout << "wed";
+            break;
+        case 5:
+            cout << "thu";
+            break;
+        case 6:
+            cout << "fri";
+            break;
+        case 7:
+            cout << "sat";
+            break;
+        default:
+            cout << "invalid day";
+            break;
     }
 }
